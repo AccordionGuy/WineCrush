4a41bb07675a9adce0d81faa050fb3cf8689f425
AccordionGuy <joey@joeydevilla.com>
2016-02-27 14:46:50 -0500

    Initial Commit

---
+++ Wine Crush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wine Crush
+//
+//  Created by Jose Martin DeVilla on 2/27/16.
+//  Copyright © 2016 Joey deVilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
